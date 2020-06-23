052dab63c4bc5d5726835d9dac151ef904edec15
Ted Neben <tedneben@gmail.com>
2020-06-23 21:58:03 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,17 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_data = data.each_with_object({}) do |(keys,values), new_hash|
+    values.each do |inner_keys, names|
+      names.each do |name|
+    if !new_hash[name]
+      new_hash[name] = {}
+    end
+    if !new_hash[name][keys]
+      !new_hash[name][keys] = []
+    end
+    new_hash[name][keys].push(inner_keys.to_s)
+    end
+   end
+  end
+ end
