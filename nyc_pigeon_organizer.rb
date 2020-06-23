da0109181441e9dac9305769a651f4d2497cbd41
Ted Neben <tedneben@gmail.com>
2020-06-23 18:53:53 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,15 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
-end
+  new_data = data.each_with_object({}) do |(keys,values), new_array|
+    value.each do |inner_keys, names|
+      names.each do |name|
+    if !new_array[name]
+      new_array[name] = {}
+    end
+    if !new_array[name][keys]
+      !new_array[name][keys] = []
+    end
+    new_array[name][keys].push[inner_keys.to_s]
+    end
+   end
+  end
+ end
